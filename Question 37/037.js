25439697352349b22b7c9e3d828611b7e6e84ce1
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-09 07:19:19 +0500

    Add files via upload

---
+++ Question 37/037.js
@@ -0,0 +1,30 @@
+//37. Large Shirts: Modify the make_shirt() function so that shirts are large
+//by default with a message that reads I love TypeScript. Make a large shirt and a
+//medium shirt with the default message, and a shirt of any size with a different
+//message.
+//
+/*function make_shirt(size:string, label: string){
+    return size + label
+}
+let myFunction = make_shirt("large", " i love typescript")
+console.log(myFunction);
+
+function make_shirt( label: string ,size:string= "large"){
+    return size + label
+}
+let myFunction = make_shirt("large", " i love typescript")
+console.log(myFunction);
+
+
+
+function make_shirt( label: string , size:string = "medium"){
+    return size + label
+}
+let myFunction = make_shirt(" i love typescript")
+console.log(myFunction);
+*/
+function make_shirt(label, size) {
+    return size + label;
+}
+var myFunction = make_shirt("  any size", " i love typescript");
+console.log(myFunction);
